42bc7aa54c8909a7f50c7de96b631e12095e08fc
takushi kawata <takushi.kawata@tokai-soft.co.jp>
2021-07-15 17:23:03 +0900

    new

---
+++ trainFileAnalyze/trainFileAnalyze/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace trainFileAnalyze
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(158, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(173, 19);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.Text = "a0";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(12, 54);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(319, 42);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "1行ずつ解析して対象データを削除する\r\nremove.trfに出力";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(14, 102);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(317, 337);
+            this.richTextBox3.TabIndex = 10;
+            this.richTextBox3.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 12);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "削除する文字と番号(例:A1)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 12);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "トレーニングファイル";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(158, 29);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(173, 19);
+            this.textBox2.TabIndex = 13;
+            this.textBox2.Text = "train_verity.trf";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(352, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ trainFileAnalyze/trainFileAnalyze/Form1.cs
@@ -0,0 +1,148 @@
+﻿using HalconDotNet;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace trainFileAnalyze
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        HObject _image;
+        HObject rectangle;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //HOperatorSet.ReadImage(out _image, "letters");
+            //hSmartWindowControl1.HalconWindow.DispObj(_image);
+        }
+
+        bool flg = false;
+        double X;
+        double Y;
+        private void hSmartWindowControl1_HMouseDown(object sender, HMouseEventArgs e)
+        {
+            //flg = true;
+            //X = e.X;
+            //Y = e.Y;
+        }
+
+        private void hSmartWindowControl1_HMouseMove(object sender, HMouseEventArgs e)
+        {
+            //if (!flg)
+            //{
+            //    return;
+            //}
+            //HOperatorSet.GenRectangle1(out rectangle, Y, X, e.Y, e.X);
+            //hSmartWindowControl1.HalconWindow.DispObj(_image);
+            //hSmartWindowControl1.HalconWindow.SetDraw("margin");
+            //hSmartWindowControl1.HalconWindow.SetColor("red");
+            //hSmartWindowControl1.HalconWindow.DispObj(rectangle);
+
+        }
+
+        private void hSmartWindowControl1_HMouseUp(object sender, HMouseEventArgs e)
+        {
+            //flg = false;
+            //hSmartWindowControl1.HMoveContent = true;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //hSmartWindowControl1.HMoveContent = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //HOperatorSet.GenImageConst(out HObject image, "byte", 9, 10);
+            //HOperatorSet.SetSystem("ocr_trainf_version", 1);
+            //HOperatorSet.WriteOcrTrainf(image, image, "a", "train_verity.trf");
+            //HOperatorSet.AppendOcrTrainf(image, image, "a", "train_verity.trf");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //HOperatorSet.ReadOcrTrainfNames("train_ocr_char_all.trf", out HTuple characterNames, out HTuple characterCount);
+
+            //StreamReader sr = new StreamReader("train_ocr_char_all.trf");
+            //string s = sr.ReadToEnd();
+            //sr.Close();
+            //richTextBox1.Text = s;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //string moji = textBox1.Text.Substring(0, 1);
+            //string iti = textBox1.Text.Substring(1, 1);
+            //bool r = Regex.IsMatch(richTextBox1.Text, moji);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string moji = textBox1.Text.Substring(0, 1);
+            int number = Convert.ToInt32(textBox1.Text.Substring(1, 1));
+            //string trainFile = "train_ocr_char_all.trf";
+            string trainFile = "train_verity.trf";
+            List<string> lines = File.ReadAllLines(trainFile).ToList();
+            List<string> removedLines = removeCharData(moji, number, lines);
+
+            if (removedLines != null)
+            {
+                File.WriteAllLines("removed.trf", removedLines.ToArray());
+                //richTextBox3.Text = result;
+            }
+
+        }
+
+        private static List<string> removeCharData(string moji, int number, List<string> lines)
+        {
+            int count = -1;
+            for (int i = 1; i < lines.Count; i++)
+            {
+                // 1行目はバージョンなので無視
+                // 文字
+                if (lines[i] == moji)
+                {
+                    count++;
+                }
+                if (count == number)
+                {
+                    int startIndex = i;
+                    // 画像　高さ
+                    int height = Convert.ToInt32(lines[++i]);
+                    i += height;
+                    // 画像　幅　高さ
+                    string[] arr = lines[++i].Split(' ');
+                    int width = Convert.ToInt32(arr[0]);
+                    height = Convert.ToInt32(arr[1]);
+                    i += height;
+                    int endIndex = i;
+                    int removeCount = endIndex - startIndex + 1;
+                    //
+                    lines.RemoveRange(startIndex, removeCount);
+
+                    return lines;
+                    //result = string.Join(Environment.NewLine, lines.ToArray());
+
+                }
+            }
+            if (count == -1)
+            {
+                MessageBox.Show("トレーニングファイルに文字が見つかりませんでした。");
+            }
+
+            return null;
+        }
+    }
+}
